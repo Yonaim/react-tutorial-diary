34585da7e47db96c1aa3877a941bf8db504aff43
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-17 19:04:37 +0900

    routing-diary에도 reducer, context 적용

--- routing-diary/src/App.js
+++ routing-diary/src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useReducer, useRef } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -11,38 +12,82 @@ import MyButton from './components/MyButton';
 import RoutesNavi from './components/RoutesNavi';
 import MyHeader from './components/MyHeader';
 
-function App() {
+const reducer = (state, action) => {
+	let newState = [];
 	
+	switch (action.type) {
+		case "INIT":
+			return action.data;
+		case "CREATE":
+			// spread 연산자를 사용하면 1단계까지는 깊은 복사됨 (내부 배열이나 객체는x)
+			const newItem = { ...action.data};
+			newState = {newItem, ...state};
+			break;
+		case "REMOVE":
+			newState = state.filter((it) => it.id !== action.targetId);
+			break;
+		case "EDIT":
+			newState = state.map((it) => it.id === action.data.id ? { ...action.data} : it);
+			break;					
+		default:
+			return state;
+	}
+	return newState;
+};
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+function App() {
+	const [data, dispatch] = useReducer(reducer, []);
+	
+	const dataId = useRef(0); // 렌더링과 상관없는 값이므로 ref 사용
+	
+	// CREATE
+	const onCreate = (date, content, emotion) => {
+		dispatch({type:"CREATE", data:{id: dataId, date: new Date(date).getTime(), content, emotion}});
+		dataId.current += 1;
+	}
+	// REMOVE
+	const onRemove = (targetId) => {
+		dispatch({type:"REMOVE", targetId});
+	}
+	// EDIT
+	const onEdit = (targetId, date, content, emotion) => {
+		dispatch({type:"EDIT", data:{id: targetId, date: new Date(date).getTime(), content, emotion}});
+	}
+
 	return (
-		<BrowserRouter>
-    	<div className="App">
-        <h1>-------- Yonazzang's Diary -------- </h1>
-      	<MyHeader headText={"헤더텍스트"}
-		leftChild={<MyButton text={"왼쪽버튼"} onClick={() => {alert("왼쪽 버튼 클릭")}}/>}
-		rightChild={<MyButton text={"오른쪽버튼"} onClick={() => {alert("오른쪽 버튼 클릭")}}/>}
-		/>
+	<DiaryStateContext.Provider value={data}>
+		{/* 최적화를 위해서는 dispatch 함수들에 대해 메모이제이션을 사용해야하지만 일단은 그냥 함 */}
+		<DiaryDispatchContext.Provider value={{onCreate, onEdit, onRemove}}>
+			<BrowserRouter>
+				<div className="App">
+				<h1>-------- Yonazzang's Diary -------- </h1>
 
-		{/* process.env.PUBLIC_URL 값이 제대로 들어있지 않은 문제 해결 필요 */}
-		{/* <img src={process.env.PUBLIC_URL + `/assets/emoticon1.png`}/> */}
-		
-		<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"positive"}/>
-		<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"negative"}/>
-		<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
-		<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"unknown"}/>
+				{/* process.env.PUBLIC_URL 값이 제대로 들어있지 않은 문제 해결 필요 */}
+				{/* <img src={process.env.PUBLIC_URL + `/assets/emoticon1.png`}/> */}
+				
+				<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"positive"}/>
+				<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"negative"}/>
+				<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+				<MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"unknown"}/>
 
-		<RoutesNavi />
-        <Routes>
-			<Route path="/" element={<Home />}/>
-			<Route path="/edit" element={<Edit />}/>
-			<Route path="/new" element={<New />}/>
-			<Route path="/diary/:id" element={<Diary />}/>
-			{/* /diary/(id)와 /diary 둘다에 대응하기 위해 보통은 이렇게 예외처리함.  */}
-			{/* <Route path="/diary" element={<Diary />}/> */}
-		</Routes>
-		{/* a태그는 새로고침 되므로 SPA 방식이 아님 */}
-		{/* <a href={"./new"}>홈으로 이동</a> */}
-      </div>
-    </BrowserRouter>
+				<RoutesNavi />
+				<Routes>
+					<Route path="/" element={<Home />}/>
+					<Route path="/edit" element={<Edit />}/>
+					<Route path="/new" element={<New />}/>
+					<Route path="/diary/:id" element={<Diary />}/>
+					{/* /diary/(id)와 /diary 둘다에 대응하기 위해 보통은 이렇게 예외처리함.  */}
+					{/* <Route path="/diary" element={<Diary />}/> */}
+				</Routes>
+				{/* a태그는 새로고침 되므로 SPA 방식이 아님 */}
+				{/* <a href={"./new"}>홈으로 이동</a> */}
+			</div>
+		</BrowserRouter>
+		</DiaryDispatchContext.Provider>
+	</DiaryStateContext.Provider>
   );
 }
 
