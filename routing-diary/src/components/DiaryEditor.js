8374b749ba48d84f3790d201dcd2d0986b6c9e73
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-18 19:08:44 +0900

    Refactor: 유틸 함수 적용해 코드 간결화

--- routing-diary/src/components/DiaryEditor.js
+++ routing-diary/src/components/DiaryEditor.js
@@ -2,41 +2,15 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { DiaryDispatchContext } from "../App";
+
+// components
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 
-const emotionList = [
-	{
-		emotion_id: 1,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-		emotion_descript: "아주 좋음",
-	},
-	{
-		emotion_id: 2,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-		emotion_descript: "좀 좋음",
-	},
-	{
-		emotion_id: 3,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-		emotion_descript: "그럭저럭",
-	},
-	{
-		emotion_id: 4,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-		emotion_descript: "별로",
-	},
-	{
-		emotion_id: 5,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-		emotion_descript: "꽝",
-	},
-];
-
-const getStringDate = (date) => {
-	return date.toISOString().slice(0, 10);
-};
+// utils
+import { getStringDate } from "../utils/date";
+import { emotionList } from "../utils/emotion";
 
 const DiaryEditor = ({ isEdit, originData }) => {
 	const [date, setDate] = useState(getStringDate(new Date()));
