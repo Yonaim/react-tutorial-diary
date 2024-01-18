14010fa7371132bbc2b27428aa6b0a3181f759f0
yonaCluster <yeonhkim@student.42seoul.kr>
2024-01-18 10:34:37 +0900

    Refactor: DiaryItem 컴포넌트 navigate 수정

--- routing-diary/src/components/DiaryItem.js
+++ routing-diary/src/components/DiaryItem.js
@@ -6,17 +6,24 @@ const DiaryItem = ({ id, emotion, content, date }) => {
 	const navigate = useNavigate();
 	const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+	const goDetail = () => {
+		navigate(`/diary/${id}`);
+	};
+	const goEdit = () => {
+		navigate(`/edit/${id}`);
+	};
+
 	return (
 		<div className="DiaryItem">
-			<div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")}>
+			<div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")} onClick={goDetail}>
 				<img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}/>
 			</div>
-			<div className="info_wrapper">
+			<div className="info_wrapper" onClick={goDetail}>
 				<div className="diary_date">{strDate}</div>
 				<div className="diary_content_preview">{content}</div>
 			</div>
 			<div>
-				<MyButton text={"수정"} onClick={() => navigate(`/edit/${id}`)}/>
+				<MyButton text={"수정"} onClick={goEdit}/>
 			</div>
 		</div>
 	); 
